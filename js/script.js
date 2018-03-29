f339e5e536c30fa10a3e9900c22d519f0f17d691
ikalchenko <ikalchenko99@gmail.com>
2018-03-29 11:03:53 +0300

    add currency switching

--- js/script.js
+++ js/script.js
@@ -1,38 +1,67 @@
-const API_KEY = 'my_key';
+const API_KEY = '24e5c179338dd7c0e8f35fe4fc5f0298';
 
 class Converter {
     constructor() {
-        this.currencyNames = Converter.getCurrencies();
+        this.symbols = Converter.getSymbols();
+        this.currencies = Converter.getCurrencies();
+    }
+
+    static getSymbols() {
+        let xhr = new XMLHttpRequest();
+        let symbols = {};
+        xhr.open('POST', 'http://www.apilayer.net/api/list?access_key=' + API_KEY, false);
+        xhr.send();
+        let JSONResponse = JSON.parse(xhr.responseText);
+        if (JSONResponse['success'] === true) {
+            for (let key in JSONResponse) {
+                if (key === 'currencies') {
+                    for (let symbol in JSONResponse[key]) {
+                        symbols[symbol + ' - ' + JSONResponse[key][symbol]] = null;
+                    }
+                }
+            }
+            localStorage.setItem('lastSymbols', JSON.stringify(symbols));
+        } else {
+            symbols = JSON.parse(localStorage.getItem('lastSymbols'));
+        }
+    
+        return symbols;
     }
 
     static getCurrencies() {
         let xhr = new XMLHttpRequest();
         let currencies = {};
-        try {
-            xhr.open('POST', 'http://www.apilayer.net/api/list?access_key=' + API_KEY, false);
-            xhr.send();
-            let JSONResponse = JSON.parse(xhr.responseText);
+        xhr.open('POST', 'http://www.apilayer.net/api/live?access_key=' + API_KEY, false);
+        xhr.send(); 
+        let JSONResponse = JSON.parse(xhr.responseText);
+        if (JSONResponse['success'] === true) {
             for (let key in JSONResponse) {
-                if (key === 'currencies') {
-                    for (let symbol in JSONResponse[key]) {
-                        currencies[symbol + ' - ' + JSONResponse[key][symbol]] = null;
+                if (key === 'quotes') {
+                    for (let currency in JSONResponse[key]) {
+                        currencies[currency] =  JSONResponse[key][currency];
                     }
                 }
             }
-        } catch (error) {
-            
+            localStorage.setItem('lastCurrencies', JSON.stringify(currencies));
+        } else {
+            currencies = JSON.parse(localStorage.getItem('lastCurrencies'));
         }
-    
         return currencies;
-    };
+    }
 }
 
 class UI {
     constructor() {
         this.currencyFrom = document.getElementById('autocomplete_from');
-        this.autocompleteFrom= M.Autocomplete.init(this.currencyFrom, {data : converter.currencyNames, minLength : 0});
         this.currencyInto = document.getElementById('autocomplete_into');
-        this.autocompleteInto = M.Autocomplete.init(this.currencyInto, {data : converter.currencyNames, minLength : 0});
+        this.switchCurrenciesBtn = document.getElementById('switch_currencies');
+        this.autocompleteFrom= M.Autocomplete.init(this.currencyFrom, {data : converter.symbols, minLength : 0});
+        this.autocompleteInto = M.Autocomplete.init(this.currencyInto, {data : converter.symbols, minLength : 0});
+    }
+    switchCurrencies() {
+        let temp = this.currencyFrom.value;
+        this.currencyFrom.value = this.currencyInto.value;
+        this.currencyInto.value = temp;     
     }
 }
 
@@ -41,6 +70,7 @@ let ui = new UI();
 
 ui.currencyFrom.addEventListener('focus', () => ui.currencyFrom.select());
 ui.currencyInto.addEventListener('focus', () => ui.currencyInto.select());
+ui.switchCurrenciesBtn.addEventListener('click', () => ui.switchCurrencies());
 
 
 
